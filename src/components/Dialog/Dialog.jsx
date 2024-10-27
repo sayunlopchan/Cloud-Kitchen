e7cc2df08c4feda24e44eb97e1598da0e99ec173
sayunlopchan <sayunlopchan01+github@gmail.com>
2024-10-27 16:19:14 +0545

    ok

--- src/components/Dialog/Dialog.jsx
+++ src/components/Dialog/Dialog.jsx
@@ -1,46 +1,48 @@
 import Lottie from "lottie-react";
-import Success from "../../assets/animation/Succes_Cart.json";
-import Error from "../../assets/animation/error.json";
+import SuccessAnimation from "../../assets/animation/Cart_Succesful.json";
+import ErrorAnimation from "../../assets/animation/error.json";
+
+
 
 const Dialog = ({ isOpen, onClose, message, isSuccess }) => {
   if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-[1000]">
-
       <div className="h-[370px] w-full md:w-[360px] rounded-lg overflow-hidden bg-white mx-5 lg:mx-0">
-
         <div className="w-full h-50 pt-3 pb-5">
-          {
-            isSuccess ? (
-              <div className="w-full h-36 rounded-t-lg flex justify-center items-center">
-                <Lottie
-                  animationData={Success}
-                  loop={false}
-                  autoplay={true}
-                  style={{ width: '220px', height: '220px' }}
-                />
-              </div>
-            ) : (
-              <div className="w-full h-36 rounded-t-lg flex justify-center items-center">
-                <Lottie
-                  animationData={Error}
-                  loop={true}
-                  autoplay={true}
-                  style={{ width: '100%', height: '100%' }}
-                />
-              </div>
-            )
-          }
+          {isSuccess ? (
+            <div className="w-full h-36 rounded-t-lg flex justify-center items-center">
+              <Lottie
+                animationData={SuccessAnimation}
+                loop={false}
+                autoplay={true}
+                style={{ width: '200px', height: '200px' }}
+              />
+            </div>
+          ) : (
+            <div className="w-full h-36 rounded-t-lg flex justify-center items-center">
+              <Lottie
+                animationData={ErrorAnimation}
+                loop={true}
+                autoplay={true}
+                style={{ width: '100%', height: '100%' }}
+              />
+            </div>
+          )}
         </div>
         <div className="w-full text-center p-3 text-gray-600">
-          {
-            isSuccess ? (
-              <span>Success! Your order has been successfully placed.<br /> Thank you for shopping with us!</span>
-            ) : (
-              <span>Oops! There was an issue processing your order. <br />  Please try again or contact support if the problem persists.</span>
-            )
-          }
+          {isSuccess ? (
+            <span>
+              Success! Your message has been sent successfully.<br />
+              Thank you for contacting us!
+            </span>
+          ) : (
+            <span>
+              Oops! There was an issue processing your message.<br />
+              Please try again or contact support if the problem persists.
+            </span>
+          )}
         </div>
         <div className="flex justify-center mt-4">
           <button onClick={onClose} className="rounded-md px-10 py-2 bg-black text-white transition-all duration-300 hover:scale-90">Close</button>
